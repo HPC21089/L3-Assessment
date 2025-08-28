adcd2ee51eb8a8236f68408ece4ee7b2b3fe5e6e
Isaac Smith <21089@haurakiplains.school.nz>
2025-08-29 10:47:19 +1200

    Start Screen Complete

--- Asessment.py
+++ Asessment.py
@@ -27,15 +27,16 @@ enemies_left = 100 #Temporary for now just to set up UI
 ultimate_charge = 0
 cooldown_charge = 0
 ultimate_status = False
+cooldown = False
 player_health = 100 #ENTIRELEY TEMP VARIABLE ONLY HERE 'TIL CLASSES
-game_state = "active"
-pause_level = 0
+game_state = "start"
+pause_level = "base"
+start_level = "base"
 arrow_limit = 0
 arrow_pos = 1
 inputs = []
 max_inputs = 4
-cooldown = False
-ultimate_status = False
+player_name = ''
 
 COMBOS = {
     'failed combo': {'name': 'Failed', 'damage': 0, 'cooldown level': 100, 'charge unit': 0},
@@ -43,11 +44,20 @@ COMBOS = {
     'combo2': {'name': 'Standard Down', 'damage' : 5, 'cooldown level': 5, 'charge unit': 10, 'input combo': ['down', 'down', 'down', 'down']},
     'combo3': {'name': 'Standard Left', 'damage': 5, 'cooldown level': 5, 'charge unit': 10, 'input combo': ['left', 'left', 'left', 'left']},
     'combo4': {'name': 'Standard Right', 'damage': 5, 'cooldown level': 5, 'charge unit': 10, 'input combo': ['right', 'right', 'right', 'right']},
-    'combo5': {'name': 'Floss', 'damage': 7, 'cooldown level': 7, 'charge unit': 15, 'input combo': ['left', 'right', 'left', 'right']},
-    'combo6': {'name': 'The Hype', 'damage': 7, 'cooldown level': 7, 'charge unit': 15, 'input combo': ['up', 'up', 'down', 'down']},
+    'combo5': {'name': 'Floss', 'damage': 7, 'cooldown level': 3.5, 'charge unit': 15, 'input combo': ['left', 'right', 'left', 'right']},
+    'combo6': {'name': 'The Hype', 'damage': 7, 'cooldown level': 3.5, 'charge unit': 15, 'input combo': ['up', 'up', 'down', 'down']},
 
     'ultimate combo': {'name': 'Ultimate Combo', 'damage': 1000000000, 'cooldown level': 1000000000, 'charge unit': 0, 'input combo': ['up', 'left', 'down', 'right']}
 }
+
+
+class Character:
+    def __init__(self, health, damage, speed):
+        self.health = health
+        self.damage = damage
+        self.speed = speed
+    
+    
 
 def draw_text(text, font, color, x, y):
     text = font.render(text, True, color)
@@ -239,6 +249,36 @@ def check_combo():
         
     return output_combo
 
+def player_pointing():
+    if not cooldown:
+        #Player Pointing Animation
+        if key_pressed[pygame.K_LEFT]:
+            player = player_pointing_left
+            player_x = 1055
+            player_y = 70
+        elif key_pressed[pygame.K_RIGHT]:
+            player = player_pointing_right
+            player_x = 1145
+            player_y = 70
+        elif key_pressed[pygame.K_UP]:
+            player = player_pointing_up
+            player_x = 1145
+            player_y = 10
+        elif key_pressed[pygame.K_DOWN]:
+            player = player_pointing_down
+            player_x = 1145
+            player_y = 70
+        else: 
+            player = player_default
+            player_x = 1145
+            player_y = 70
+    else:
+        player = player_default
+        player_x = 1145
+        player_y = 70
+
+    WINDOW.blit(player, (player_x, player_y))
+
 #-----DISPLAY GAME UI-----
 def ui_blit(key_pressed):
     """Displays all parts of the base game UI"""
@@ -256,6 +296,8 @@ def ui_blit(key_pressed):
         draw_text("Enemies Left: " + str(enemies_left), font_medium, WHITE, 545, 687) #CHANGE X POS FOR SPACING, 2 didgits
     elif enemies_left <= 9:
         draw_text("Enemies Left: " + str(enemies_left), font_medium, WHITE, 545, 687) #CHANGE X POS FOR SPACING, single digit
+
+    draw_text(player_name, font_small, WHITE, 1135, 15)
 
     #Esc to pause
     WINDOW.blit(esc_bg, (25, 25))
@@ -277,6 +319,8 @@ def ui_blit(key_pressed):
     #-----Health Bar-----
     hb_blit()    
         
+    player_pointing()
+
 #DISPLAY PAUSE SCREEN
 def pause_screen(arrow_limit):
     #Gray overlay
@@ -290,7 +334,7 @@ def pause_screen(arrow_limit):
     draw_text("to play", font_small, WHITE, 70, 35)
     draw_text("Z to confirm", font_small, WHITE, 31, 75)
 
-    if pause_level == 1:
+    if pause_level == "base":
         #Selection Text
         draw_text("ENEMIES", font_big, WHITE, 562.5, 135)
         draw_text("MOVES", font_big, WHITE, 577.5, 265)
@@ -310,6 +354,49 @@ def pause_screen(arrow_limit):
 
     return arrow_limit
 
+#Display start screen
+def start_screen(arrow_limit):
+    #Gray overlay
+    overlay = pygame.Surface((1280, 720), pygame.SRCALPHA)  
+    overlay.fill((128, 128, 128, 150))
+    WINDOW.blit(overlay, (0, 0))
+
+    #Logo
+    WINDOW.blit(logo, (280, 100))
+
+    draw_text("Z to confirm", font_small, WHITE, 592.5, 660)
+    draw_text("X to uhhh, do the opposite or whatever", font_small, WHITE, 483, 680)
+    draw_text("Arrow keys to move to arrow", font_small, WHITE, 520, 700)
+
+    if start_level == "base":
+        draw_text("NEW PLAYER", font_big, WHITE, 525.5, 475)
+        draw_text("RETURNING PLAYER", font_big, WHITE, 467.5, 575)
+
+        arrow_limit = 2
+        
+        if arrow_pos == 1:
+            WINDOW.blit(selection_arrow, (443, 465))
+        elif arrow_pos == 2:
+            WINDOW.blit(selection_arrow, (385, 565))
+
+    if start_level == "new player":
+        draw_text("INPUT NAME:", font_big, WHITE, 527.5, 475)
+        draw_text("BACK", font_big, WHITE, 590, 600)
+        
+        draw_text(player_name, font_big, WHITE, 525.5, 525)
+
+        if player_name != '':
+            draw_text("Enter to confirm", font_small, WHITE, 756, 489)
+
+        arrow_limit = 2
+
+        if arrow_pos == 1:
+            WINDOW.blit(selection_arrow, (444, 465))
+        elif arrow_pos == 2:
+            WINDOW.blit(selection_arrow, (507.5, 590))
+
+    return arrow_limit
+
 blank = pygame.transform.scale_by(pygame.image.load('images/Blank.png'), 5)
 
 #Loading background
@@ -318,12 +405,9 @@ background = pygame.transform.scale_by(pygame.image.load('images/DDDZA-Backgroun
 #-----Loading the gauge sprites-----#
 gauge_0 = pygame.transform.scale_by(pygame.image.load('images/gauges/Gauge-0%.png'), 5)
 hb_empty = pygame.transform.scale_by(pygame.image.load('images/health bar/HB-Empty.png'), 5)
-
-#Ultimate Charge sprites (abbreviated with 'uc')
 ultimate_charge_images = load_uc_images()
-
-#Cooldown sprites (abbreviated with 'c')
 cooldown_charge_images = load_c_images()
+hb_images = load_hb_images()
 
 #Loading arrows
 up_arrow_base = pygame.transform.scale_by(pygame.image.load('images/arrows/up-arrow.png'), 5)
@@ -334,9 +418,6 @@ left_arrow_base = pygame.transform.scale_by(pygame.image.load('images/arrows/lef
 left_arrow_pressed = pygame.transform.scale_by(pygame.image.load('images/arrows/left-arrow-pressed.png'), 5)
 right_arrow_base = pygame.transform.scale_by(pygame.image.load('images/arrows/right-arrow.png'), 5)
 right_arrow_pressed = pygame.transform.scale_by(pygame.image.load('images/arrows/right-arrow-pressed.png'), 5)
-
-#Loading in health bar (abbreviated to HB)
-hb_images = load_hb_images()
 
 #Loading Escape Image
 esc_bg = pygame.transform.scale_by(pygame.image.load('images/esc.png'), 5)
@@ -357,37 +438,33 @@ player_pointing_down = pygame.transform.scale_by(pygame.image.load('images/Playe
 player_pointing_left = pygame.transform.scale_by(pygame.image.load('images/Player/Pointing/Player-Pointing-Left.png'), 5)
 player_pointing_right = pygame.transform.scale_by(pygame.image.load('images/Player/Pointing/Player-Pointing-Right.png'), 5)
 
+logo = pygame.transform.scale_by(pygame.image.load('images/Logo.png'), 5)
+
 #Game loop
 if __name__ == "__main__":
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                exit()
-            
-            if event.type == pygame.KEYUP:#Setting the switch for changing game states + resetting variables
-                if event.key == pygame.K_ESCAPE:
-                    if game_state == "active":
-                        game_state = "paused"
-                        pause_level = 1
-                        arrow_pos = 1
-                    elif game_state == "paused":
-                        game_state = "active"
-                        pause_level = 0
-                        arrow_pos = 1
+                exit()                
 
-            if game_state == "paused":#Selection arrow movement
-                #Selection arrow movement
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_DOWN:
-                        arrow_pos += 1
-                    elif event.key == pygame.K_UP:
-                        arrow_pos -= 1
-
-            if game_state == "active": #Combo input tracking, Executing a combo, Clearing combo list, Limiting no. of recorded inputs
-                if not cooldown:
-                    #Combo input tracking
-                    if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
+                
+                if game_state == "start":
+                    if start_level == "new player":
+                        if arrow_pos == 1:
+                            if event.key == pygame.K_BACKSPACE:
+                                player_name = player_name[:-1]
+                            elif event.key == pygame.K_RETURN:
+                                pass
+                            else:
+                                if len(player_name) < 6:
+                                    player_name += event.unicode
+                
+                if game_state == "active":
+                    if not cooldown:
+                        
+                        #Combo input tracking
                         if event.key == pygame.K_LEFT:
                             inputs.append('left')
                         if event.key == pygame.K_RIGHT:
@@ -407,10 +484,14 @@ if __name__ == "__main__":
                                 else:
                                     output_combo = 'failed combo'
                             else:
-                                inputs = []
-                                ultimate_charge += COMBOS[output_combo]['charge unit']
-                                cooldown = True
-                                cooldown_charge = 100
+                                if output_combo == 'failed combo':
+                                    inputs = []
+                                else:
+                                    inputs = []
+                                    ultimate_charge += COMBOS[output_combo]['charge unit']
+                                    cooldown = True
+                                    cooldown_charge = 100
+                        
                         #Clearing combo list
                         if event.key == pygame.K_x:
                             inputs = []
@@ -418,52 +499,92 @@ if __name__ == "__main__":
                 #Limits the number of recorded inpits at a time to 0
                 if len(inputs) > max_inputs:
                     inputs.pop(0)
-                    
+
+            if event.type == pygame.KEYUP:
+                
+                #Switching between all screens
+                if event.key == pygame.K_z:
+                    if game_state == "start":
+                        if start_level == "base":
+                            if arrow_pos == 1:
+                                start_level = "new player"
+                            if arrow_pos == 2:
+                                start_level = "returning"
+                        if start_level == "new player":
+                            if arrow_pos == 2:
+                                start_level = "base"   
+                    if game_state == "paused":
+                        if pause_level == "base":
+                            if arrow_pos == 1:
+                                pause_state = "enemies"
+                            if arrow_pos == 2:
+                                pause_state = "moves"
+                            if arrow_pos == 3:
+                                pause_state = "achievements"
+                            if arrow_pos == 4:
+                                game_state = "start"
+                                start_level = "base"
+                                arrow_pos = 1
+                
+                if event.key == pygame.K_x:
+                    if game_state == "start":
+                        if start_level == "new player" and arrow_pos == 2 or start_level == "returning":
+                            start_level = "base"
+                    if game_state == "paused":
+                        if pause_level == "enemies" or pause_level == "moves" or pause_level == "achievements":
+                            pause_level = "base"
+                
+                if event.key == pygame.K_ESCAPE:
+                    if game_state == "active":
+                        game_state = "paused"
+                        pause_level = "base"
+                        arrow_pos = 1
+                    elif game_state == "paused":
+                        game_state = "active"
+                        pause_level = "base"
+                        arrow_pos = 1
+                if event.key == pygame.K_RETURN:
+                    if start_level == "new player":
+                            if arrow_pos == 1 and player_name != '':
+                                game_state = "active"
+
+                #Selection arrow movement
+                if game_state == "paused" or game_state == "start":
+                    if event.key == pygame.K_DOWN:
+                        arrow_pos += 1
+                    elif event.key == pygame.K_UP:
+                        arrow_pos -= 1
+                         
         key_pressed = pygame.key.get_pressed()
-        
-        if not cooldown: #Player Pointing Animation
-            #Player Pointing Animation
-            if key_pressed[pygame.K_LEFT]:
-                player = player_pointing_left
-                player_x = 1055
-                player_y = 70
-            elif key_pressed[pygame.K_RIGHT]:
-                player = player_pointing_right
-                player_x = 1145
-                player_y = 70
-            elif key_pressed[pygame.K_UP]:
-                player = player_pointing_up
-                player_x = 1145
-                player_y = 10
-            elif key_pressed[pygame.K_DOWN]:
-                player = player_pointing_down
-                player_x = 1145
-                player_y = 70
-            else: 
-                player = player_default
-                player_x = 1145
-                player_y = 70
 
         WINDOW.blit(background, (0,0))
         
-        WINDOW.blit(player, (player_x, player_y))
+        #Keeping the selection arrow only on 
+        if arrow_pos > arrow_limit:
+            arrow_pos = 1
+        elif arrow_pos <= 0:
+            arrow_pos = arrow_limit
 
+        #Checking to enable ultimate status
         if ultimate_charge >= 100:
             ultimate_status = True
         else: 
             ultimate_status = False
 
-        if game_state == "active": #Blit game ui
+        if game_state == "start":
+            #Blit start screen
+            arrow_limit = start_screen(arrow_limit)
+
+        elif game_state == "active":
+            #Blit game ui 
             ui_blit(key_pressed)
+
+            #Cooldown animation
             cooldown, cooldown_charge = on_cooldown(cooldown, cooldown_charge)
-        elif game_state == "paused": #Blit pause screen, restrict arrow
+        
+        elif game_state == "paused":
             #Blit pause screen
             arrow_limit = pause_screen(arrow_limit)
-            #Restricts arrow to only on screen options
-            if arrow_pos > arrow_limit:
-                arrow_pos = arrow_limit
-            elif arrow_pos <= 0:
-                arrow_pos = 1
         
         CLOCK.tick(FRAME_RATE)
         pygame.display.update()
